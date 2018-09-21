c5d0b9b809fe90f94954cbf9cb6fe1521f5f63ae
Jonathan Payne <jonathan.b.payne@gmail.com>
2018-09-21 14:48:29 -0400

    Added Reverse Guess The Number game

---
+++ reverseGuess.js
@@ -0,0 +1,34 @@
+const readline = require('readline');
+const readlineInterface = readline.createInterface(
+  { input: process.stdin, output: process.stdout }
+);
+
+const ask = questionText => new Promise((resolve, reject) => {
+  readlineInterface.question(questionText, resolve);
+});
+
+let number = Math.ceil(Math.random()*100);
+
+const start = async () => {
+
+  while (true) {
+    let playerGuess = await ask(`Enter your guess: `);
+
+        if (playerGuess < number) {
+      console.log("Higher");
+    }
+    if (playerGuess > number) {
+      console.log("Lower");
+    }
+    if (playerGuess == number) {
+      console.log(`Yes, ${number} is correct!`);
+      process.exit();
+    }
+
+  }
+}
+
+console.log("I'm thinking of a number between 1 and 100.  Can you guess the number?");
+
+
+start();
